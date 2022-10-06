1f64b1b6cc923493359cc4392d76351d83e52257
Azat Harutyunyan <azat.harutyunyan@eemi.com>
2022-10-06 18:03:04 +0200

    formation presque finis

---
+++ src/components/Error/index.jsx
@@ -0,0 +1,44 @@
+import styled from "styled-components";
+import colors from "../../utils/style/colors";
+import { useTheme } from "../../utils/hooks";
+import banger from "../../assets/banger.jpg";
+
+const ErrorWrapper = styled.div`
+  margin: 30px;
+  display: flex;
+  flex-direction: column;
+  background-color: ${({ theme }) =>
+    theme === "light" ? colors.backgroundLight : colors.backgroundDark};
+  align-items: center;
+`;
+
+const ErrorTitle = styled.h1`
+  color: ${({ theme }) => (theme === "light" ? "#000000" : "#ffffff")};
+  font-weight: 300;
+`;
+
+const ErrorSubtitle = styled.h2`
+  color: ${({ theme }) => (theme === "light" ? colors.secondary : "#ffffff")};
+  font-weight: 300;
+`;
+
+const Illustration = styled.img`
+  max-width: 800px;
+`;
+
+function Error() {
+  const { theme } = useTheme();
+
+  return (
+    <ErrorWrapper theme={theme}>
+      <ErrorTitle theme={theme}>Oups...</ErrorTitle>
+      <Illustration src={banger} />
+      <ErrorSubtitle theme={theme}>
+        Il semblerait que la page que vous cherchez n’existe pas, mais vous
+        pouvez lire Vagabond si vous le souhaitez
+      </ErrorSubtitle>
+    </ErrorWrapper>
+  );
+}
+
+export default Error;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,44 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./pages/Home";
+import Survey from "./pages/Survey";
+import Results from "./pages/Results";
+import Freelances from "./pages/Freelances";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Error from "./components/Error";
+import GlobalStyle from "./utils/style/GlobalStyle";
+import { ThemeProvider, SurveyProvider } from "./utils/context";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Router>
+      <ThemeProvider>
+        <SurveyProvider>
+          <GlobalStyle />
+          <Header />
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/survey/:questionNumber">
+              <Survey />
+            </Route>
+            <Route path="/results">
+              <Results />
+            </Route>
+            <Route path="/freelances">
+              <Freelances />
+            </Route>
+            <Route path="*">
+              <Error />
+            </Route>
+          </Switch>
+          <Footer />
+        </SurveyProvider>
+      </ThemeProvider>
+    </Router>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
